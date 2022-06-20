598c2ad8e79cc007541873b2eb366db8cd589f15
Matheus Costa Barboza <matheus10s10@gmail.com>
2022-06-20 20:48:32 -0300

    Update Aplicacao.java

--- campo-minado/src/br/com/campoMinado/Aplicacao.java
+++ campo-minado/src/br/com/campoMinado/Aplicacao.java
@@ -4,7 +4,7 @@ import br.com.campoMinado.modelo.Tabuleiro;
 import br.com.campoMinado.visao.TabuleiroConsole;
 
 public class Aplicacao {
-	//main
+	
 	public static void main(String[] args) {
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
 		new TabuleiroConsole(tabuleiro);
