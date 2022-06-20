747134a919971c110ea7d4a56787c747cc3e44e9
Matheus Costa Barboza <matheus10s10@gmail.com>
2022-06-20 20:26:56 -0300

    Commit Inicial
    Projeto do campo minado no console do Java

---
+++ campo-minado/src/br/com/campoMinado/Aplicacao.java
@@ -0,0 +1,16 @@
+package br.com.campoMinado;
+
+import br.com.campoMinado.modelo.Tabuleiro;
+import br.com.campoMinado.visao.TabuleiroConsole;
+
+public class Aplicacao {
+	
+	public static void main(String[] args) {
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		new TabuleiroConsole(tabuleiro);
+		
+		
+		
+	}
+	
+}
---
+++ campo-minado/src/br/com/campoMinado/excecao/ExplosaoException.java
@@ -0,0 +1,8 @@
+package br.com.campoMinado.excecao;
+
+public class ExplosaoException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+
+
+}
